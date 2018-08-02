6c5fe81bdd9bfd7e26581db860560e74ed892dab
Daniel Nieto <danielnieto89@gmail.com>
2018-08-02 12:09:02 -0500

    add placeholder AudioPlayer widget

---
+++ src/js/audio-player.js
@@ -0,0 +1,8 @@
+Class("AudioPlayer").inherits(Widget)({
+    prototype: {
+        element: $("<h1>Audio Player</h1>"),
+        init: function() {
+            console.log("initialized");
+        }
+    }
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
-var myWidgetInstance = new Widget();
-myWidgetInstance.element.html("Im a simple widget");
-myWidgetInstance.render(document.querySelector("#app"));
+require("./audio-player");
+
+new AudioPlayer().render(document.body);
